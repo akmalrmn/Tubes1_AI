0f3549dce0be6416a3fa2133091e66d471b0b9fb
JasonFernandoo <j450n.fernando@gmail.com>
2024-11-04 20:16:22 +0700

    refactor: update frontend

--- frontend/src/result/Result.jsx
+++ frontend/src/result/Result.jsx
@@ -9,7 +9,29 @@ const Result = () => {
                 <h1>Result</h1>
             </div>
             <div className='result-content'>
-
+                <div className='duration'>
+                  <p>Duration: </p>
+                </div>
+                <div className='objective'>
+                  <p>Objective: </p>
+                </div>
+                <div className='plot'>
+                  <div className='plot-left'>
+                    <p>Plot: </p>
+                  </div>
+                  <div className='plot-right'>
+                    <p>Plot eET: </p>
+                  </div>
+                </div>
+                <div className='iteration'>
+                  <p>Iteration: </p>
+                </div>
+                <div className='frequency'>
+                  <p>Frequency:  </p>
+                </div>
+                <div className='population'>
+                  <p>Population: </p>
+                </div>
             </div>
         </div>
     </div>
