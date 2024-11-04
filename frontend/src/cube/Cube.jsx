59d63b0d2610d8df04f557948afdbee8dfd57a7f
Mohammad Akmal Ramadan <118426250+akmalrmn@users.noreply.github.com>
2024-11-04 23:01:57 +0700

    feat: final state for cube

--- frontend/src/cube/Cube.jsx
+++ frontend/src/cube/Cube.jsx
@@ -1,11 +1,16 @@
-import { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../cube/Cube.css';
-import { runSimulatedAnnealing } from '../services/api';
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "../cube/Cube.css";
+import {
+  runSimulatedAnnealing,
+  runGeneticAlgorithm,
+  runSteepestAscent,
+} from "../services/api";
 
 const Cube = () => {
   const [rotation, setRotation] = useState({ x: -30, y: -45 });
-  const [cubeData, setCubeData] = useState(null);
+  const [initialCubeData, setInitialCubeData] = useState(null);
+  const [finalCubeData, setFinalCubeData] = useState(null);
   const cubeRef = useRef(null);
   const touchRef = useRef({ x: 0, y: 0 });
   const isDragging = useRef(false);
@@ -14,7 +19,8 @@ const Cube = () => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await runSimulatedAnnealing();
-      setCubeData(data.initialState.Cube.Tables);
+      setInitialCubeData(data.initialState.Cube.Tables);
+      setFinalCubeData(data.finalState.Cube.Tables);
     };
     fetchData();
   }, []);
@@ -22,7 +28,7 @@ const Cube = () => {
   const handleTouchStart = (e) => {
     touchRef.current = {
       x: e.touches[0].clientX,
-      y: e.touches[0].clientY
+      y: e.touches[0].clientY,
     };
   };
 
@@ -31,12 +37,12 @@ const Cube = () => {
     const deltaY = e.touches[0].clientY - touchRef.current.y;
     touchRef.current = {
       x: e.touches[0].clientX,
-      y: e.touches[0].clientY
+      y: e.touches[0].clientY,
     };
 
     setRotation((prevRotation) => ({
       x: prevRotation.x - deltaY * 0.5,
-      y: prevRotation.y + deltaX * 0.5
+      y: prevRotation.y + deltaX * 0.5,
     }));
   };
 
@@ -44,7 +50,7 @@ const Cube = () => {
     isDragging.current = true;
     touchRef.current = {
       x: e.clientX,
-      y: e.clientY
+      y: e.clientY,
     };
   };
 
@@ -55,12 +61,12 @@ const Cube = () => {
     const deltaY = e.clientY - touchRef.current.y;
     touchRef.current = {
       x: e.clientX,
-      y: e.clientY
+      y: e.clientY,
     };
 
     setRotation((prevRotation) => ({
       x: prevRotation.x - deltaY * 0.5,
-      y: prevRotation.y + deltaX * 0.5
+      y: prevRotation.y + deltaX * 0.5,
     }));
   };
 
@@ -72,155 +78,186 @@ const Cube = () => {
     setRotation({ x: -30, y: -45 });
     setTimeout(() => {
       setTimeout(() => {
-        navigate('/expand');
+        navigate("/expand");
       }, 1000);
     }, 0);
   };
 
-  const getFaceData = (face) => {
+  const getFaceData = (cubeData, face) => {
     if (!cubeData) return null;
-    
-    const size = cubeData[0][0].length;
-    
-    switch(face) {
-      case 'front':
+
+    const size = cubeData[0].length;
+
+    switch (face) {
+      case "front":
         return cubeData[0];
-      
-      case 'back':
-        return cubeData[4].map((row, rowIndex) => 
-          row.map((_, colIndex) => cubeData[4][rowIndex][colIndex]).reverse()
-        );
-      
-      case 'right':
-        return cubeData[0].map((_, colIndex) => 
-          cubeData.map((table) => table[colIndex][size - 1])
-        );
-      
-      case 'left':
-        return cubeData[0].map((_, colIndex) => 
-          cubeData.map((table) => table[colIndex][0]).reverse()
-        );
-      
-      case 'top':
-        return cubeData.map(table => table[0]).reverse();
-      
-      case 'bottom':
-        return cubeData.map(table => table[size - 1]);
-      
+      case "back":
+        return cubeData[4];
+      case "left":
+        return cubeData.map((row) => row[0]);
+      case "right":
+        return cubeData.map((row) => row[size - 1]);
+      case "top":
+        return cubeData.map((row, idx) => cubeData[idx][0]);
+      case "bottom":
+        return cubeData.map((row, idx) => cubeData[idx][size - 1]);
       default:
         return null;
     }
   };
 
-  const faces = ['front', 'back', 'left', 'right', 'top', 'bottom'];
+  const faces = ["front", "back", "left", "right", "top", "bottom"];
 
-  const handleAlgClick = (e) => {
-    document.querySelectorAll('.alg-button button').forEach(button => {
-      button.classList.remove('active');
+  const handleAlgClick = async (e) => {
+    // Remove 'active' class from all buttons and add to clicked button
+    document.querySelectorAll(".alg-button button").forEach((button) => {
+      button.classList.remove("active");
     });
-    e.target.classList.add('active');
+    e.target.classList.add("active");
+
+    
+    let data;
+    if (e.target.id === "geneticAlgorithm") {
+      data = await runGeneticAlgorithm(50, 100);
+    } else if (e.target.id === "simulatedAnnealing") {
+      data = await runSimulatedAnnealing();
+    } else if (e.target.id === "steepestAscent") {
+      data = await runSteepestAscent(1000, 25);
+    }
+
+    // Update cube data if data is successfully fetched
+    if (data) {
+      setInitialCubeData(data.initialState.Cube.Tables);
+      setFinalCubeData(data.finalState.Cube.Tables);
+    }
   };
 
   return (
-    <div className='cube-container'>
-      <div className='container'>
-        <div className='top-container'>
-          <div className='left-container'>
-              <div className='title'>
-                <h1>Initial Cube</h1>
-              </div>
-              <div className='cube-box'>
-                <div className='cube-wrapper'>
+    <div className="cube-container">
+      <div className="container">
+        <div className="top-container">
+          <div className="left-container">
+            <div className="title">
+              <h1>Initial Cube</h1>
+            </div>
+            <div className="cube-box">
+              <div className="cube-wrapper">
+                <div
+                  className={`cube`}
+                  onTouchStart={handleTouchStart}
+                  onTouchMove={handleTouchMove}
+                  onMouseDown={handleMouseDown}
+                  onMouseMove={handleMouseMove}
+                  onMouseUp={handleMouseUp}
+                  onMouseLeave={handleMouseUp}
+                >
                   <div
-                    className={`cube`}
-                    onTouchStart={handleTouchStart}
-                    onTouchMove={handleTouchMove}
-                    onMouseDown={handleMouseDown}
-                    onMouseMove={handleMouseMove}
-                    onMouseUp={handleMouseUp}
-                    onMouseLeave={handleMouseUp}
+                    className="main-cube"
+                    ref={cubeRef}
+                    style={{
+                      transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
+                    }}
                   >
-                    <div
-                      className='main-cube'
-                      ref={cubeRef}
-                      style={{ transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)` }}
-                    >
-                      {cubeData && faces.map((face, faceIdx) => (
-                        <div key={faceIdx} className={`cube-face cube-face-${face}`}>
-                          <div className='grid-container'>
-                            {getFaceData(face)?.map((row, rowIdx) => (
-                              <div key={rowIdx} className='grid-row'>
-                                {Array.isArray(row) ? row.map((cell, cellIdx) => (
-                                  <div key={cellIdx} className='grid-cells'>
-                                    {cell}
-                                  </div>
-                                )) : (
-                                  <div key={rowIdx} className='grid-cells'>
-                                    {row}
-                                  </div>
-                                )}
-                              </div>
-                            ))}
+                    {initialCubeData &&
+                      faces.map((face, faceIdx) => (
+                        <div
+                          key={faceIdx}
+                          className={`cube-face cube-face-${face}`}
+                        >
+                          <div className="grid-container">
+                            {getFaceData(initialCubeData, face)?.map(
+                              (row, rowIdx) => (
+                                <div key={rowIdx} className="grid-row">
+                                  {Array.isArray(row) ? (
+                                    row.map((cell, cellIdx) => (
+                                      <div key={cellIdx} className="grid-cell">
+                                        {cell}
+                                      </div>
+                                    ))
+                                  ) : (
+                                    <div key={rowIdx} className="grid-cell">
+                                      {row}
+                                    </div>
+                                  )}
+                                </div>
+                              )
+                            )}
                           </div>
                         </div>
                       ))}
-                    </div>
                   </div>
                 </div>
               </div>
+            </div>
           </div>
-          <div className='right-container'>
-              <div className='title'>
-                <h1>Final Cube</h1>
-              </div>
-              <div className='cube-box'>
-              <div className='cube-wrapper'>
+          <div className="right-container">
+            <div className="title">
+              <h1>Final Cube</h1>
+            </div>
+            <div className="cube-box">
+              <div className="cube-wrapper">
+                <div
+                  className={`cube`}
+                  onTouchStart={handleTouchStart}
+                  onTouchMove={handleTouchMove}
+                  onMouseDown={handleMouseDown}
+                  onMouseMove={handleMouseMove}
+                  onMouseUp={handleMouseUp}
+                  onMouseLeave={handleMouseUp}
+                >
                   <div
-                    className={`cube`}
-                    onTouchStart={handleTouchStart}
-                    onTouchMove={handleTouchMove}
-                    onMouseDown={handleMouseDown}
-                    onMouseMove={handleMouseMove}
-                    onMouseUp={handleMouseUp}
-                    onMouseLeave={handleMouseUp}
+                    className="main-cube"
+                    ref={cubeRef}
+                    style={{
+                      transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
+                    }}
                   >
-                    <div
-                      className='main-cube'
-                      ref={cubeRef}
-                      style={{ transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)` }}
-                    >
-                      {cubeData && faces.map((face, faceIdx) => (
-                        <div key={faceIdx} className={`cube-face cube-face-${face}`}>
-                          <div className='grid-container'>
-                            {getFaceData(face)?.map((row, rowIdx) => (
-                              <div key={rowIdx} className='grid-row'>
-                                {Array.isArray(row) ? row.map((cell, cellIdx) => (
-                                  <div key={cellIdx} className='grid-cells'>
-                                    {cell}
-                                  </div>
-                                )) : (
-                                  <div key={rowIdx} className='grid-cells'>
-                                    {row}
-                                  </div>
-                                )}
-                              </div>
-                            ))}
+                    {finalCubeData &&
+                      faces.map((face, faceIdx) => (
+                        <div
+                          key={faceIdx}
+                          className={`cube-face cube-face-${face}`}
+                        >
+                          <div className="grid-container">
+                            {getFaceData(finalCubeData, face)?.map(
+                              (row, rowIdx) => (
+                                <div key={rowIdx} className="grid-row">
+                                  {Array.isArray(row) ? (
+                                    row.map((cell, cellIdx) => (
+                                      <div key={cellIdx} className="grid-cell">
+                                        {cell}
+                                      </div>
+                                    ))
+                                  ) : (
+                                    <div key={rowIdx} className="grid-cell">
+                                      {row}
+                                    </div>
+                                  )}
+                                </div>
+                              )
+                            )}
                           </div>
                         </div>
                       ))}
-                    </div>
                   </div>
                 </div>
               </div>
+            </div>
           </div>
         </div>
-        <div className='bottom-container'>
-          <div className='alg-button'>
-            <button id="geneticAlgorithm" onClick={handleAlgClick}>Genetic Algorithm</button>
-            <button id="simulatedAnnealing" onClick={handleAlgClick}>Simulated Annealing</button>
-            <button id="steepestAscent" onClick={handleAlgClick}>Steepest Ascent</button>
+        <div className="bottom-container">
+          <div className="alg-button">
+            <button id="geneticAlgorithm" onClick={handleAlgClick}>
+              Genetic Algorithm
+            </button>
+            <button id="simulatedAnnealing" onClick={handleAlgClick}>
+              Simulated Annealing
+            </button>
+            <button id="steepestAscent" onClick={handleAlgClick}>
+              Steepest Ascent
+            </button>
           </div>
-          <div className='expand-button'>
+          <div className="expand-button">
             <button onClick={handleExpandClick}>Expand</button>
           </div>
         </div>
